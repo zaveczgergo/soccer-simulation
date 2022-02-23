9a67418aed93e0794bf48c139bef40aa2e74fac4
zaveczgergo <gergo.zavecz@gmail.com>
2022-02-23 19:36:52 +0100

    putting a function into the ml file. Closes #3

--- analysis_data.py
+++ analysis_data.py
@@ -15,69 +15,43 @@ from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.svm import LinearSVC
 from sklearn import metrics
 
+def ml_reg(model, var): 
+    
+    errors = {}
+    coefs = {}
+    
+    dependent = data[var]
+    
+    x_train, x_test, y_train, y_test = train_test_split(independent, dependent, test_size = 0.2, random_state = 0)
+    
+    reg = model
+    reg.fit(x_train, y_train)
+    
+    y_pred_train = reg.predict(x_train)
+    y_pred_test = reg.predict(x_test)
+    
+    errors["mae_train"] = metrics.mean_absolute_error(y_train, y_pred_train)
+    errors["mse_train"] = metrics.mean_squared_error(y_train, y_pred_train)
+    errors["rmse_train"] = np.sqrt(metrics.mean_squared_error(y_train, y_pred_train))
+    errors["mae_test"] = metrics.mean_absolute_error(y_test, y_pred_test)
+    errors["mse_test"] = metrics.mean_squared_error(y_test, y_pred_test)
+    errors["rmse_test"] = np.sqrt(metrics.mean_squared_error(y_test, y_pred_test))
+    
+    print(errors)
+    print(reg.coef_)
+    #return coefs
+
 data = pd.read_csv("output/analysis_sample.csv", index_col = 0, low_memory = False)
-
-data = pd.concat([data.loc[:,"Acceleration_y":"Volleys"], data.loc[:,"Acceleration_y_own":"Volleys_other"], data.loc[:,"assist"]], axis = 1)
+print(data.columns)
+data = pd.concat([data.loc[:,"Acceleration_y":"Volleys"], data.loc[:,"Acceleration_y_own":"Volleys_other"], data.loc[:,"assist":"clearance"]], axis = 1)
 independent = data.loc[:,"Acceleration_y":"Volleys_other"]
-#independent = data_nonmissing.loc[:,"Acceleration_y":"Volleys_other"]
-dependent = data["assist"]
 
-X_train, X_test, y_train, y_test = train_test_split(independent, 
-                                                    dependent, 
-                                                    test_size = 0.2, 
-                                                    random_state = 0)
+print(data.columns)
 
-print("Training set has {} cases.".format(X_train.shape[0]))
-print("Testing set has {} cases.".format(X_test.shape[0]))
+model_A = LinearRegression()
+model_B = Ridge(alpha=0.5)
+model_C = Lasso()
 
-clf_A = LinearRegression()
-reg = clf_A.fit(X_train, y_train)
-y_pred = clf_A.predict(X_test)
-y_pred_train = clf_A.predict(X_train)
-print(clf_A.coef_)
-#print(reg.get_params())
-
-print('Mean Absolute Error_test:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error_test:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error_test:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-
-print('Mean Absolute Error_train:', metrics.mean_absolute_error(y_train, y_pred_train))
-print('Mean Squared Error_train:', metrics.mean_squared_error(y_train, y_pred_train))
-print('Root Mean Squared Error_train:', np.sqrt(metrics.mean_squared_error(y_train, y_pred_train)))
-
-clf_B = Ridge(alpha=0.5)
-reg_r = clf_B.fit(X_train, y_train)
-y_pred_r = clf_B.predict(X_test)
-#y_train = clf_A.predict(X_train)
-print(clf_B.coef_)
-#print(reg.get_params())
-
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred_r))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred_r))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_r)))
-
-clf_C = Lasso()
-reg_l = clf_C.fit(X_train, y_train)
-y_pred_l = clf_C.predict(X_test)
-#y_train = clf_A.predict(X_train)
-print(clf_C.coef_)
-coefs = dict(zip(independent.columns, clf_C.coef_.tolist()))
-print(coefs)
-print(type(clf_C.coef_.tolist()))
-#print(reg.get_params())
-
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred_l))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred_l))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_l)))
-
-#clf_D = LinearSVC()
-#reg_s = clf_D.fit(X_train, y_train)
-#y_pred_s = clf_D.predict(X_test)
-#y_train = clf_A.predict(X_train)
-#print(clf_D.coef_)
-#print(reg.get_params())
-
-#print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred_l))
-#print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred_l))
-#print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred_l)))
-#print(data["playerank_score"].corr(data["Overall"]))
+for m in [model_A, model_B, model_C]:
+    for d in ["assist", "shot", "pass", "interception", "foul"]:
+        ml_reg(m, d)
