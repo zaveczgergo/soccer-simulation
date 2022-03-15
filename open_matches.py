3e474932b9b1a7973c965d832525cd04caa0edcc
zaveczgergo <gergo.zavecz@gmail.com>
2022-03-15 16:23:48 +0100

    Correcting open_matches.py, Closes #1

--- open_matches.py
+++ open_matches.py
@@ -14,38 +14,52 @@ data_all = []
 def open_json_matches(file):
     df = pd.read_json(file)
     print(df.shape)
-  
+      
     df["teamsData_teams"] = df["teamsData"].apply(lambda x: list(x.keys()))
-    df["teamsData_teams_home"] = df["teamsData"].apply(lambda x: list(x.keys())[0])
-    df["teamsData_teams_away"] = df["teamsData"].apply(lambda x: list(x.keys())[1])
-    print(df.shape)
     
-    teams_dict = df["teamsData"].values
-    teams_dict_agg = []
-    for e in teams_dict:
-        e_df = pd.DataFrame.from_dict(e, orient="index")
-        teams_dict_agg.append(e_df)
-    teams_dict_df = pd.concat(teams_dict_agg)
-    teams_dict_df["lineup"] = teams_dict_df["formation"].apply(lambda x: x["lineup"])
-    players_list = []
-    for i in range(11):
-        players = teams_dict_df["lineup"].apply(lambda x: x[i]["playerId"])
-        players_list.append(players)
-    players_df = pd.concat(players_list, axis = 1)
-    players_df = pd.concat([teams_dict_df["side"],players_df], axis = 1)
-    players_df["onelist"] = players_df.iloc[:,1:12].apply(lambda x: list(x), axis = 1)
-    players_df = players_df.loc[:,["side","onelist"]].reset_index()
-    players_df['match'] = (players_df.index / 2 + 1).astype(int)
-    players_df = players_df.pivot(index = "match", columns = "side", values = ["index", "onelist"]).reset_index()
-    players_df.columns = map(lambda x: ".".join([str(i) for i in x]).strip(), players_df.columns)
-    players_df = players_df.iloc[:, 1:5]
-    players_df.columns = ["teamsData_teams_away", "teamsData_teams_home", "players_away", "players_home"]
-    players_df = players_df.iloc[:, 2:4]
-    data = pd.concat([df,players_df], axis = 1)
-    data = data.loc[:, ["teamsData_teams_home", "teamsData_teams_away", "players_home", "players_away", "wyId"]]
-    print(data.shape)
+    df["team_1"] = df["teamsData"].apply(lambda x: list(x.keys())[0])
+    df["team_2"] = df["teamsData"].apply(lambda x: list(x.keys())[1])
+    
+    df["side_1"] = df["teamsData"].apply(lambda x: list(x.items())[0][1].get("side"))
+    df["side_2"] = df["teamsData"].apply(lambda x: list(x.items())[1][1].get("side"))
+    
+    df["players_1"] = df["teamsData"].apply(lambda x: list(x.items())[0][1].get("formation").get("lineup"))
+    df["players_2"] = df["teamsData"].apply(lambda x: list(x.items())[1][1].get("formation").get("lineup"))
+    
+    df_1 = df.explode("players_1")
+    df_1["player_id_1"] = df_1["players_1"].apply(lambda x: x.get("playerId"))
+    df_1_match = df_1.groupby(by = ["wyId"])[["team_1","side_1"]].first()
+    df_1_players = df_1.groupby(by = ["wyId"])["player_id_1"].apply(list)
+    
+    df_2 = df.explode("players_2")
+    df_2["player_id_2"] = df_2["players_2"].apply(lambda x: x.get("playerId"))
+    df_2_match = df_2.groupby(by = ["wyId"])[["team_2","side_2"]].first()
+    df_2_players = df_2.groupby(by = ["wyId"])["player_id_2"].apply(list)
+    
+    df_match = pd.merge(df_1_players, df_2_players, how = "inner", on = "wyId")
+    df_match = pd.merge(df_match, df_1_match, how = "inner", on = "wyId")
+    df_match = pd.merge(df_match, df_2_match, how = "inner", on = "wyId")
+    df_match = df_match.reset_index()
+    print(df_match.shape)
+    print(df_match.index)
+    
+    df_match["teamsData_teams_home"] = df_match["team_1"]
+    df_match.loc[df_match["side_1"] == "away", "teamsData_teams_home"] = df_match["team_2"]
+    
+    df_match["teamsData_teams_away"] = df_match["team_2"]
+    df_match.loc[df_match["side_1"] == "away", "teamsData_teams_away"] = df_match["team_1"]
+    
+    df_match["players_home"] = df_match["player_id_1"]
+    df_match.loc[df_match["side_1"] == "away", "players_home"] = df_match["player_id_2"]
+    
+    df_match["players_away"] = df_match["player_id_2"]
+    df_match.loc[df_match["side_1"] == "away", "players_away"] = df_match["player_id_1"]
+    
+    df_match.drop(["player_id_1", "player_id_2", "team_1", "side_1", "team_2", "side_2"], inplace = True, axis = 1)
+
+    print(df_match.shape)
  
-    data_all.append(data)
+    data_all.append(df_match)
         
     #file_name = file.split('.')[0]
     #output = file_name + ".csv" 
